4356f635b2b8777daac34415c79c458bf59ffcbd
gourabop3 <gourabmullick200@gmail.com>
2025-07-29 20:22:55 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,6 @@
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from "@/generated/prisma";
 
-//* This file creates a Prisma Client and attaches it to the global object so that only one instance of the client is created in your application. This helps resolve issues with hot reloading that can occur when using Prisma ORM with Next.js in development mode.
-
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient()
 
